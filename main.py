9349eee9492da4c3590e1287aa73587a96aa27f0
Bink <78453289+PHenrq@users.noreply.github.com>
2022-06-15 06:21:25 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from kivy.lang import Builder
+from kivy.app import App
+import requests
+
+GUI = Builder.load_file("main.kv")
+
+
+class MeuApp(App):
+
+    def build(self):
+        return GUI
+
+    def on_start(self):
+        self.root.ids["moeda1"].text = f"Dolar R$: {self.pegar_cotacao('USD')}"
+        self.root.ids["moeda2"].text = f"Euro R$: {self.pegar_cotacao('EUR')}"
+        self.root.ids["moeda3"].text = f"Bitcoin R$: {self.pegar_cotacao('BTC')}"
+        self.root.ids["moeda4"].text = f"Ethereum R$: {self.pegar_cotacao('ETH')}"
+
+    def pegar_cotacao(self, moeda):
+        link = f"https://economia.awesomeapi.com.br/last/{moeda}-BRL"
+        requisicao = requests.get(link)
+        dic_requisicao = requisicao.json()
+        cotacao = dic_requisicao[f"{moeda}BRL"]["bid"]
+        return cotacao
+
+
+MeuApp().run()
